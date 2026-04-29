5e9b4f436138a8e370a3716395bedf75e0b2a7a8
fabricio rafael <142851247+fabriciorafael@users.noreply.github.com>
2026-04-28 22:56:40 -0300

    setup inicial javascript

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const products = [
+
+];
+
+function createProductCard(product) {
+    return `
+    <div class="product-card">
+          <img src="${product.image}" alt="${product.title}" class="product-image">
+          <h3 class="product-title"></h3>
+         <div class="product-price"></div>
+          <div class="product-discount"></div>       
+    </div>
+    `
+
+}
+
+function renderProduct(){
+    
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+
+})
