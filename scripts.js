8d9706e55b204f79dd50a5f6511f7e8cb4cb0cb4
fabricio rafael <142851247+fabriciorafael@users.noreply.github.com>
2026-05-04 22:06:34 -0300

    feat: continuação da implementação em javascript

--- scripts.js
+++ scripts.js
@@ -2,20 +2,30 @@ const products = [
 
 ];
 
+function formatPrice(price) {
+    return price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+}
+
+
+
+
 function createProductCard(product) {
     return `
     <div class="product-card">
           <img src="${product.image}" alt="${product.title}" class="product-image">
-          <h3 class="product-title"></h3>
-         <div class="product-price"></div>
-          <div class="product-discount"></div>       
+          <h3 class="product-title">${product.title}</h3>
+         <div class="product-price">${product.price}</div>
+          <div class="product-discount">${product.discont}% OFF</div>       
     </div>
     `
 
 }
 
-function renderProduct(){
-    
+function renderProduct(products) {
+    const grid = document.getElementsById('productsGrid')
+
+    grid.innerHTML = products.map(createProductCard).join('')
+
 }
 
 
